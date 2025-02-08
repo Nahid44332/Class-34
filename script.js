3c82784377da09ecdb36be031d417accc4344df3
Nahid44332 <nahidhossen3278@gmail.com>
2025-02-07 19:28:41 -0800

    Javascript-statement

---
+++ script.js
@@ -0,0 +1,34 @@
+let number = 456545;
+
+if (number > 0){
+    console.log ("the number is positive");
+}
+
+else if (number < 0){
+    console.log("the number is nagetive");
+}
+
+//writte a js program to find Maximum between two Number.
+
+53 , 86
+
+let number2 = 86;
+let number3 = 53;
+
+if(number2 > number3){
+    console.log ("Number2 is Maximum");
+}
+else if(number2 < number3){
+    console.log("Number3 is Maximum");
+}
+
+//writte a js program to chech whether a numbner is divisible by 5.
+
+let number4 = 9;
+
+if(number4 % 3 == 0){
+    console.log("The Number is Division by 5");
+}
+else {
+    console.log("The Number is not Division by 5");
+}
